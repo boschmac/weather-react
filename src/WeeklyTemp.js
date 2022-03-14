daad91f95927eba76509a9cd3e7ddf1f7244dfdf
boschmac <90581512+boschmac@users.noreply.github.com>
2022-03-13 21:37:21 -0400

    updated class to className in missing spots

--- src/WeeklyTemp.js
+++ src/WeeklyTemp.js
@@ -13,40 +13,40 @@ export default function WeeklyTemp() {
         <div className="card-body">
         <h5>Mon</h5>
         <p className="card-text weather-emoji"> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" /> </p>
-        <p class="card-text">12°C</p>
-				<p class="card-text">-5°C</p>
+        <p className="card-text">12°C</p>
+				<p className="card-text">-5°C</p>
         </div>
       </div>
       <div className="col-2">
         <div className="card-body">
         <h5>Tues</h5>
         <p className="card-text weather-emoji"> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" /> </p>
-        <p class="card-text">12°C</p>
-				<p class="card-text">-5°C</p>
+        <p className="card-text">12°C</p>
+				<p className="card-text">-5°C</p>
         </div>
       </div>
       <div className="col-2">
         <div className="card-body">
         <h5>Wed</h5>
         <p className="card-text weather-emoji"> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" /> </p>
-        <p class="card-text">12°C</p>
-				<p class="card-text">-5°C</p>
+        <p className="card-text">12°C</p>
+				<p className="card-text">-5°C</p>
         </div>
       </div>
       <div className="col-2">
         <div className="card-body">
         <h5>Thur</h5>
         <p className="card-text weather-emoji"> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" /> </p>
-        <p class="card-text">12°C</p>
-				<p class="card-text">-5°C</p>
+        <p className="card-text">12°C</p>
+				<p className="card-text">-5°C</p>
         </div>
       </div>
       <div className="col-2">
         <div className="card-body">
         <h5>Fri</h5>
         <p className="card-text weather-emoji"> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" /> </p>
-        <p class="card-text">12°C</p>
-				<p class="card-text">-5°C</p>
+        <p className="card-text">12°C</p>
+				<p className="card-text">-5°C</p>
         </div>
       </div>
       
